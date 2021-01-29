a1e897349fce79cb6920279c2f954342f74041db
Gabriel Ferreira <gferreiradossantos123@gmail.com>
2021-01-28 22:59:12 -0300

    Primeiro commit

---
+++ src/database/fakedata.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633",
+        lng: "-49.6455874",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+
+            "https://images.unsplash.com/photo-1609917846910-9d2a9bde4bd1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+
+            "https://images.unsplash.com/photo-1605798286922-482b37219d6b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-27.222633",
+        lng: "-49.6555874",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+
+            "https://images.unsplash.com/photo-1609917846910-9d2a9bde4bd1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+
+            "https://images.unsplash.com/photo-1605798286922-482b37219d6b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+    },
+]
---
+++ src/database/test.js
@@ -0,0 +1,35 @@
+const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage');
+
+Database.then(async db => {
+
+    // inserir dados na tabela
+    await saveOrphanage(db,{
+        lat: "-27.222633",
+        lng: "-49.6555874",
+        name: "Lar dos meninos",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "98989887898",
+        images: [
+            "https://images.unsplash.com/photo-1609917846910-9d2a9bde4bd1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+
+            "https://images.unsplash.com/photo-1605798286922-482b37219d6b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+        ].toString(),
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+    })
+
+    // // constular dados da tabela
+     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
+     console.log(selectedOrphanages)
+
+    // consultar somente 1 orphanato, pelo id
+    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
+    console.log(orphanage)
+
+    // // deletar dado da tabela
+    // console.log(await db.run("DELETE FROM orphanages WHERE id = '4'"))
+    // console.log(await db.run("DELETE FROM orphanages WHERE id = '5'"))
+    // console.log(await db.run("DELETE FROM orphanages WHERE id = '6'"))
+})
